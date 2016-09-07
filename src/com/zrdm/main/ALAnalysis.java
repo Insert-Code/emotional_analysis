558787d4ce2144776568c4884dfa628943a114a3
Insert-Code <136589072@qq.com>
2016-09-07 20:24:20 +0800

    v0.2

--- src/com/zrdm/main/ALAnalysis.java
+++ src/com/zrdm/main/ALAnalysis.java
@@ -3,47 +3,95 @@ package com.zrdm.main;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.sun.media.sound.AlawCodec;
 import com.zrdm.db.DBBean;
 
 /**
  * 
- * @author zrdm
- * 情感词汇分析
+ * @author zrdm 情感词汇分析
  * 
  */
 public class ALAnalysis {
-	
-	private String words = "喜欢"; 
+
+	private String word = "";
 	private String sql = "";
-	private int score = 0 ; // 情感强度
-	private int jixing = 0; //极性 0代表中性，1代表褒义，2代表贬义，3代表兼有褒贬两性。
+	private int single_score = 0;// 单个词的情感强度
+	private int score; // 总情感强度
+	private int jixing = 0; // 极性 0代表中性，1代表褒义，2代表贬义，3代表兼有褒贬两性。
+	private DBBean db = null;// 数据库连接；
+	private int semparent; // 关联词汇id
+	private String semrelate = ""; // 关联词汇的关系
+	private boolean word_Negative = false; // 默认否定词不存在
+	private int word_Negative_num = 1; // 分析否定情况
+	private String[] negative_Array = { "不", "不好", "否", "不许", "无", "勿", "毋",
+			"非", "莫", "禁止", "禁", "防止", "杜绝", "拒绝" };
 
-	public ALAnalysis(){
-		DBBean db = new DBBean();
-		sql = "select * from Alontology where 词语 = ?";
-		ResultSet rs = db.executeQuery_Pr(sql, words);
-		try {
-			while (rs.next()) {
-				System.out
-						.println(rs.getString("词语") + " "
-								+ rs.getString("词性种类") + " "
-								+ rs.getString("情感分类") + " "
-								+ rs.getInt("强度") + " " 
-								+ rs.getInt("极性") + " " 
-								+ rs.getString("辅助情感分类") + " "
-								+ rs.getInt("强度1") + " "
-								+ rs.getInt("极性1"));
+	public ALAnalysis() throws Exception {
+		alAnalysis();
+	}
+
+	public void alAnalysis() throws Exception {
+		score = 0;
+		db = new DBBean();
+		SplitStatement sps = new SplitStatement();
+		sql = "select * from weibo;";
+		ResultSet rs1 = db.executeQuery(sql);
+		while (rs1.next()) {
+			String text = rs1.getString("博文内容");
+			sps.setText(text);
+			sps.analysis();
+
+			sql = "select * from Alontology where 词语 = ?;";
+
+			JSONArray jsonArray = new JSONArray(sps.getJsonStr());
+			JSONObject jsonObj = null;
+
+			for (int i = 0; i < jsonArray.length(); i++) {
+				word_Negative = false; // 初始化否定词会不存在；
+				jsonObj = jsonArray.getJSONObject(i);
+
+				word = jsonObj.getString("cont");
+				semrelate = jsonObj.getString("semrelate");
+
+				System.out.println("++ " + word + "++");
+				ResultSet rs = db.executeQuery_Pr(sql, word);
+				for (int j = 0; j < negative_Array.length; j++) {
+					// 如果有否定词，且该否定词为另一个词的否定标记，取得另一个词的id;
+					if ((word.equals(negative_Array[j]) || word == negative_Array[j])
+							&& (semrelate.equals("mNeg") || semrelate == "mNeg")) {
+						semparent = jsonObj.getInt("semparent");
+						word_Negative = true;
+						word_Negative_num *= -1;
+					}
+				}
+				if (word_Negative == true) {
+					continue;
+				}
+				while (rs.next()) {
+					if (word == rs.getString("词语")
+							|| word.equals(rs.getString("词语"))) {
+						single_score = rs.getInt("强度") * word_Negative_num;
+						if (rs.getInt("极性") == 2) {
+							single_score = single_score * -1;
+						}
+						System.out.println("single_score = " + single_score);
+						score += single_score;
+
+					}
+				}
+
 			}
-
-		} catch (SQLException e) {
-			e.printStackTrace();
+			System.out.println("score = " + score);
 		}
-				
 	}
-	
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws Exception {
 		ALAnalysis al = new ALAnalysis();
+
 	}
-	
+
 }
--- src/com/zrdm/main/SplitStatement.java
+++ src/com/zrdm/main/SplitStatement.java
@@ -1,17 +1,70 @@
 package com.zrdm.main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.sun.media.sound.AlawCodec;
+
 /**
  * 
- * @author zrdm
- * 拆分语句
- * 分词
- *
+ * @author zrdm 拆分语句 分词
+ * 
  */
 public class SplitStatement {
-	private String texts = "";
 	
-	public SplitStatement(){
-		
+	private String api_key = "C7J8P5E7DgOkFsiK4KCdyEwNapjcPWgBJPhJ6BxK"; //API密钥
+	private String pattern = "all"; //模式
+	private String format = "json"; //输出格式json or xml
+	private String text = ""; //分词语句	
+	private String jsonStr = "";  // json格式的分词结果
+
+	public SplitStatement(){}
+	
+	public void analysis() throws Exception{
+		jsonStr = "";
+		text = URLEncoder.encode(text, "utf-8");
+		URL url = new URL("http://api.ltp-cloud.com/analysis/?" + "api_key="
+				+ api_key + "&" + "text=" + text + "&" + "format=" + format
+				+ "&" + "pattern=" + pattern);
+		URLConnection conn = url.openConnection();
+		conn.connect();
+
+		BufferedReader innet = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));
+		String line = "";
+		int i = 0;
+		while ((line = innet.readLine()) != null) {
+			if(i++ < 2 && (line.trim().equals("[") || line == "[")){
+				continue;
+			}
+			jsonStr += line;
+		}		
+	
+		jsonStr = jsonStr.substring(0,jsonStr.length()-2);
+		innet.close();
+	}
+	
+	public String getText() {
+		return text;
 	}
 
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	public String getJsonStr() {
+		return jsonStr;
+	}
+
+	public void setJsonStr(String jsonStr) {
+		this.jsonStr = jsonStr;
+	}
+	
 }
